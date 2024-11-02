507be7bbf1bdc4fc8c91f8e1f37a8f8cb73aefc7
iLinx007 <ahiadulincoln@gmail.com>
2024-11-01 21:32:27 -0400

    Initial commit

--- app.mjs
+++ app.mjs
@@ -3,7 +3,15 @@ import path from 'path'
 import { fileURLToPath } from 'url';
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.listen(process.env.PORT || 3000);
+app.get('/', (req, res) => {
+    res.send('Game Backlog Tracker');
+  });
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${port}`);
+});
